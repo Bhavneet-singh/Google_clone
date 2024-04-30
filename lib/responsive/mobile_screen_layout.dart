0e44ae62e52ddbfbf792c2ac77af35b99c3d1412
BhavHacker <109847685+BhavHacker@users.noreply.github.com>
2024-04-30 22:30:27 +0530

    web done

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,13 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_clone/colors.dart';
+import 'package:google_clone/search.dart';
+import 'package:google_clone/widgets/searchbuttons.dart';
+import 'package:google_clone/widgets/translations.dart';
 
 class MobileScreen extends StatelessWidget {
   const MobileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Mobile screen'),
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Gmail',
+                style:
+                    TextStyle(color: primaryColor, fontWeight: FontWeight.w400),
+              )),
+          TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Images',
+                style:
+                    TextStyle(color: primaryColor, fontWeight: FontWeight.w400),
+              )),
+          const SizedBox(
+            width: 10,
+          ),
+          IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                'images/more-apps.svg',
+                color: Colors.white,
+              )),
+          const SizedBox(
+            width: 10,
+          ),
+          MaterialButton(
+            onPressed: () {},
+            color: Color(0xff1A73EB),
+            child: const Text("Sign in "),
+          ),
+          const SizedBox(
+            width: 10,
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 5, right: 5),
+        child: Column(
+          children: [
+            SizedBox(
+              height: (size.height) * 0.15,
+            ),
+            const Expanded(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Search(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SearchButtons(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Translations()
+                  ],
+                ),
+              ],
+            )),
+          ],
+        ),
       ),
     );
   }
